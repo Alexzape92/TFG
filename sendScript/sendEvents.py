ad7620264d92fcc53057966db52f6d50c9fa5e45
Alexzape92 <alexguitarte@gmail.com>
2024-06-03 18:49:17 +0200

    feat: finished script. Not tested yet

--- sendScript/sendEvents.py
+++ sendScript/sendEvents.py
@@ -1,9 +1,45 @@
 from confluent_kafka import Producer
 import socket
+import random
+import time
+import json
 
 config = {
     'bootstrap.servers': 'localhost:9092',
     'client.id': socket.gethostname()
 }
-
 producer = Producer(config)
+
+nEventsToSend = 100
+simpleEventsSchema = {
+    'HospitalFridgeTemp': {
+        'value': {
+            'min': -4,
+            'max': 10
+        },
+        'unit': 'C'
+    }
+}
+sensorId = "sensor1"
+
+def get_event(eventSchema, timestamp, sensorId) -> dict:
+    event = {'timestamp': timestamp, 'sensorId': sensorId}
+
+    for key in eventSchema:
+        value = eventSchema[key]
+        if isinstance(value, dict):
+            event[key] = random.uniform(value['min'], value['max'])
+        else:
+            event[key] = value
+    
+    return event
+
+def init_simulation():
+    for key in simpleEventsSchema:
+        for i in range(nEventsToSend):
+            event = get_event(simpleEventsSchema[key], int(time.time()), sensorId)
+            producer.produce(key, key="event {0}".format(i), value=json.dumps(event))
+    producer.flush()
+
+if __name__ == "__main__":
+    init_simulation()
